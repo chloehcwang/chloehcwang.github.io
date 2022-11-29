a6f1ddc62432132637bc68ac7b1652aa7e6da65e
allen-code <cliu403@gmail.com>
2022-11-29 20:55:13 +1300

    chore: add pb link

--- source/js/script.js
+++ source/js/script.js
@@ -6,12 +6,19 @@
     function (event) {
       let navbar_height;
       if (window.scrollY > 100) {
+        // gastric page
         document.getElementById("navbar-gastric").classList.add("fixed-top");
+        // pbtech page
+        document.getElementById("navbar-pbtech").classList.add("fixed-top");
+
         // add padding top to show content behind navbar
         navbar_height = document.querySelector(".navbar").offsetHeight;
         document.body.style.paddingTop = navbar_height + "px";
       } else {
+        // gastric page
         document.getElementById("navbar-gastric").classList.remove("fixed-top");
+        // pbtech page
+        document.getElementById("navbar-pbtech").classList.remove("fixed-top");
         // remove padding top from body
         document.body.style.paddingTop = "0";
       }
