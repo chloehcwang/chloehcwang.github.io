e2de3971da033a5a0e60dd71b6f85d12eb9f6b3e
allen-code <cliu403@gmail.com>
2023-02-28 21:56:55 +1300

    chore: static nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
-
+  // gastric page
   document.addEventListener(
     "scroll",
     function (event) {
@@ -8,6 +8,24 @@
       if (window.scrollY > 100) {
         // gastric page
         document.getElementById("navbar-gastric").classList.add("fixed-top");
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector(".navbar").offsetHeight;
+        document.body.style.paddingTop = navbar_height + "px";
+      } else {
+        // gastric page
+        document.getElementById("navbar-gastric").classList.remove("fixed-top");
+        // remove padding top from body
+        document.body.style.paddingTop = "0";
+      }
+    },
+    true /*Capture event*/
+  );
+  // pbtech page
+  document.addEventListener(
+    "scroll",
+    function (event) {
+      let navbar_height;
+      if (window.scrollY > 100) {
         // pbtech page
         document.getElementById("navbar-pbtech").classList.add("fixed-top");
 
@@ -15,8 +33,6 @@
         navbar_height = document.querySelector(".navbar").offsetHeight;
         document.body.style.paddingTop = navbar_height + "px";
       } else {
-        // gastric page
-        document.getElementById("navbar-gastric").classList.remove("fixed-top");
         // pbtech page
         document.getElementById("navbar-pbtech").classList.remove("fixed-top");
         // remove padding top from body
--- theme/js/script.js
+++ theme/js/script.js
@@ -6,7 +6,7 @@
 
 (function () {
   "use strict";
-
+  // gastric page
   document.addEventListener(
     "scroll",
     function (event) {
@@ -14,6 +14,24 @@
       if (window.scrollY > 100) {
         // gastric page
         document.getElementById("navbar-gastric").classList.add("fixed-top");
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector(".navbar").offsetHeight;
+        document.body.style.paddingTop = navbar_height + "px";
+      } else {
+        // gastric page
+        document.getElementById("navbar-gastric").classList.remove("fixed-top");
+        // remove padding top from body
+        document.body.style.paddingTop = "0";
+      }
+    },
+    true /*Capture event*/
+  );
+  // pbtech page
+  document.addEventListener(
+    "scroll",
+    function (event) {
+      let navbar_height;
+      if (window.scrollY > 100) {
         // pbtech page
         document.getElementById("navbar-pbtech").classList.add("fixed-top");
 
@@ -21,8 +39,6 @@
         navbar_height = document.querySelector(".navbar").offsetHeight;
         document.body.style.paddingTop = navbar_height + "px";
       } else {
-        // gastric page
-        document.getElementById("navbar-gastric").classList.remove("fixed-top");
         // pbtech page
         document.getElementById("navbar-pbtech").classList.remove("fixed-top");
         // remove padding top from body
